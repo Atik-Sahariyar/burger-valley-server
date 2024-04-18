1343f4862eeff98f20accfc9002fe9ac4b4be263
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-04-18 21:09:50 +0600

    Server setup with folder structure

---
+++ src/apis.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+// Import all routers
+const routers = [
+
+];
+
+// Dynamically apply routers
+routers.forEach((route) => router.use(route));
+
+module.exports = router;
