e50f1aff5b04ec0fd5d883eaa3e14d1f2cc543aa
parvejme24 <mdparvejme24@gmail.com>
2024-04-20 23:13:18 +0600

    add get product by category controller and router

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -10,6 +10,17 @@ exports.getAllProducts = async (req, res) => {
   }
 };
 
+// Get products by category
+exports.getProductsByCategory = async (req, res) => {
+  try {
+    const { category } = req.params;
+    const products = await Product.find({ category });
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 // get product by id
 exports.getProductById = async (req, res) => {
   try {
--- src/routers/ProductRoutes.js
+++ src/routers/ProductRoutes.js
@@ -4,6 +4,7 @@ const productController = require("../controllers/ProductController");
 
 productRouter.get("/products", productController.getAllProducts);
 productRouter.get("/products/:id", productController.getProductById);
+productRouter.get("/products/category/:category", productController.getProductsByCategory);
 productRouter.put("/products/:id", productController.updateProductById);
 productRouter.delete("/products/:id", productController.deleteProductById);
 
